0edda58ea913eb8dd038104f5eb1f2924034091b
sandeep.n <58665444+sandy1709@users.noreply.github.com>
2020-12-22 14:35:07 +0530

    Update download.py

--- userbot/plugins/download.py
+++ userbot/plugins/download.py
@@ -35,7 +35,7 @@ async def _(event):
                     progress(d, t, mone, c_time, "trying to download")
                 ),
             )
-        except Exception as e:  # pylint:disable=C0103,W0703
+        except Exception as e: 
             await mone.edit(str(e))
         else:
             end = datetime.now()
@@ -92,6 +92,43 @@ async def _(event):
             await mone.edit("Incorrect URL\n {}".format(input_str))
     else:
         await mone.edit("Reply to a message to download to my local server.")
+
+
+@bot.on(admin_cmd(pattern="dlto(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="dlto(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    pwd = os.getcwd()
+    input_str = event.pattern_match.group(1)
+    if not input_str:
+        return await edit_delete(event, "Where should i save this file. mention folder name" ,parse_mode=parse_pre)
+    location = os.path.join(pwd , input_str)
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    if event.reply_to_msg_id:
+        mone = await edit_or_reply(event, "Downloading the file ...",parse_mode=parse_pre)
+        start = datetime.now()
+        reply_message = await event.get_reply_message()
+        try:
+            c_time = time.time()
+            downloaded_file_name = await event.client.download_media(
+                reply_message,
+                location,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(d, t, mone, c_time, "trying to download")
+                ),
+            )
+        except Exception as e:
+            await mone.edit(str(e),parse_mode=parse_pre)
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+            await mone.edit(
+                f"**  •  Downloaded in {ms} seconds.**\n**  •  Downloaded to :- ** `{downloaded_file_name}`\n**  •  Downloaded by :-** {DEFAULTUSER}"
+            )
+    else:
+        await mone.edit("Reply to a message to download to my server.",parse_mode=parse_pre)        
 
 
 CMD_HELP.update(
