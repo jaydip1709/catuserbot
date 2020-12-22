bb3bcccc9d5bc5b866ce80b3fd557c785175bc7a
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-12-22 20:44:58 +0000

    pylint: auto fixes

--- userbot/plugins/fileconverts.py
+++ userbot/plugins/fileconverts.py
@@ -1,16 +1,16 @@
 # by @mrconfused (@sandy1709)
 
 import asyncio
+import base64
 import os
 import time
 from datetime import datetime
 from io import BytesIO
-import base64
+
 from telethon import functions, types
 from telethon.errors import PhotoInvalidDimensionsError
-from telethon.tl.functions.messages import SendMediaRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-
+from telethon.tl.functions.messages import SendMediaRequest
 
 from . import make_gif, progress
 
