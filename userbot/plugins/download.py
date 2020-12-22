19993af8f3980c7d32b771a708eb73d85ccb4713
sandy1709 <58665444+sandy1709@users.noreply.github.com>
2020-12-22 13:00:30 +0000

    pylint: auto fixes

--- userbot/plugins/download.py
+++ userbot/plugins/download.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 from pySmartDL import SmartDL
 
-from . import ALIVE_NAME, humanbytes, progress, rumcmd
+from . import ALIVE_NAME, humanbytes, progress
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
